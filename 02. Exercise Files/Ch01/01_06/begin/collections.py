a43e9281d4de8f48e51d4e14ea1e96efb6eb20a6
MD. SHAHRIAR AHAMMAD <callbackcrows7@gmail.com>
2024-10-19 14:07:31 +0600

    Learning Python- Career Essentials in Software Development

---
+++ 02. Exercise Files/Ch01/01_06/begin/collections.py
@@ -0,0 +1,19 @@
+# Nearest stars to Earth
+stars = [
+    'Sol',
+    'Alpha Centauri',
+    'Barnard',
+    'Wolf 359',
+]
+
+# Highest peak on each tectonic plate
+peak = {
+    'African' : 'Kilimanjaro',
+    'Antarctic' : 'Vinson',
+    'Australian' : 'Puncak Jaya',
+    'Eurasian' : 'Everest',
+    'North_American' : 'Denali',
+    'Pacific' : 'Mauna Kea',
+    'South_American' : 'Aconcagua',
+}
+
